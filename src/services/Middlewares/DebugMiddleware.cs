caa52541f22a4739574bbd8d2ffa9ffd1f9565f4
Asif Raja <asifraja@Asifs-iMac.local>
2020-02-29 22:24:35 +0000

    init service

---
+++ src/services/Middlewares/DebugMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace services.Middlewares
+{
+    public class DebugMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public DebugMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next(context);
+            // Inspecting or updating response
+            var newBody = new MemoryStream();
+            //newBody.Seek(0, SeekOrigin.Begin);
+            //var newContent = new StreamReader(newBody).ReadToEnd();
+            var newContent = ", Copyright (c) 2020 Mindmakr Limited.";
+            await context.Response.WriteAsync(newContent);
+        }
+    }
+
+    public static class DebugMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseDebug(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<DebugMiddleware>();
+        }
+    }
+}
---
+++ src/services/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using services.Middlewares;
+
+namespace services
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDebug();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRequestCulture();
+            app.UseRouting();
+            app.Map("/map1", HandleMapTest1);
+            app.Map("/map2", HandleMapTest2);
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+
+
+        private static void HandleMapTest1(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Map Test 1");
+            });
+        }
+
+        private static void HandleMapTest2(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Map Test 2");
+            });
+        }
+    }
+}
